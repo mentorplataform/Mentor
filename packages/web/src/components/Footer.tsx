4c4123ca64d08155b9422d48e3b8de9abd8ca2b3
Wellerson <wellerson.moreira@intelie.com.br>
2021-01-17 00:38:23 -0300

    add: footer component

---
+++ packages/web/src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Footer: React.FC = () => {
+  return (
+      <Container>
+          footer
+      </Container>
+  )
+}
+
+export default Footer;
+
+const Container = styled.footer`
+  background: rgba(254, 178, 34, 0.6);
+  height: 150px;
+  width: 100%;
+`
