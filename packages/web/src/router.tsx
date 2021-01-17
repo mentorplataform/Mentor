dc168c1452f85316c39bf6e6cff75270d129aa0e
viniciusanchieta <vinicius.anchieta@outlook.com>
2021-01-17 13:46:57 -0300

    Exchange items finished

--- packages/web/src/router.tsx
+++ packages/web/src/router.tsx
@@ -13,6 +13,7 @@ const Course = lazy(() => import('pages/Course' /* webpackChunkName: "Course" */
 const CourseConfirmed = lazy(() => import('pages/CourseConfirmed' /* webpackChunkName: "Course" */))
 const CourseInfo = lazy(() => import('pages/CourseInfo' /* webpackChunkName: "Course-infomation" */))
 const SubscriptionSuccessfully = lazy(() => import('pages/SubscriptionSuccessfully' /* webpackChunkName: "Subscription-successfully" */))
+const ExchangeItems = lazy(() => import('pages/ExchangeItems' /* webpackChunkName: "Subscription-successfully" */))
 
 const router = () => {
 
@@ -36,6 +37,7 @@ const router = () => {
                                 <Route path="/curso-confirmado" component={CourseConfirmed} />
                                 <Route path="/informacao-curso" component={CourseInfo} />
                                 <Route path="/inscricao-confirmada" component={SubscriptionSuccessfully} />
+                                <Route path="/troca-item" component={ExchangeItems} />
                                 {/* <Route path="/register" component={Register} />
                                 <Route path="/forgot" component={Forgot} /> */}
                                 {/* <PrivateRouter path="/profile" component={Profile} />
